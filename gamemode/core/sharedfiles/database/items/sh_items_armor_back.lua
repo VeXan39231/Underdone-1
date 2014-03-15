2d685c053b92c8bf9bce8d66370fa942ce893ef8
VeXan39231 <parkerno552@yahoo.com>
2014-03-15 00:53:48 -0500

    Added 10 Back Sacks!

--- gamemode/core/sharedfiles/database/items/sh_items_armor_back.lua
+++ gamemode/core/sharedfiles/database/items/sh_items_armor_back.lua
@@ -14,10 +14,82 @@ local function AddBuff(tblAddTable, strBuff, intAmount)
 	return tblAddTable
 end
 
-local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack", "Item Sack", "Up the amount of things you can carry", "icons/amor_junkyarmor")
-Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0))
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack", "Back Sack", "Upgrades your storage-space by 10 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(0, 255, 0, 255))
 Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 10)
 Item.Weight = 0
+Item.SellPrice = 100
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack2", "Back Sack 2", "Upgrades your storage-space by 20 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(0, 0, 255, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 20)
+Item.Weight = 0
 Item.SellPrice = 200
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack3", "Back Sack 3", "Upgrades your storage-space by 30 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(0, 0, 255, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack4", "Back Sack 4", "Upgrades your storage-space by 40 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(255, 0, 255, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack5", "Back Sack 5", "Upgrades your storage-space by 50 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(255, 153, 0, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack6", "Back Sack 6", "Upgrades your storage-space by 60 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(255, 255, 0, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack7", "Back Sack 7", "Upgrades your storage-space by 70 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(0, 255, 255, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack8", "Back Sack 8", "Upgrades your storage-space by 80 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(120, 0, 255, 255))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack9", "Back Sack 9", "Upgrades your storage-space by 90 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0), Color(0, 0, 0, 255), Material("models/shiny"))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
+Register.Item(Item)
+
+local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack10", "Ultimate Back Sack", "Upgrades your storage-space by 100 slots.", "icons/amor_junkyarmor")
+Item = AddModel(Item, "models/props_c17/TrapPropeller_Engine.mdl", Vector(0, -10, 10), Angle(-95.9, -10.5, 0))
+Item = AddStats(Item, "slot_back", 0)
+Item = AddBuff(Item, "stat_maxweight", 30)
+Item.Weight = 0
+Item.SellPrice = 300
 Register.Item(Item)
