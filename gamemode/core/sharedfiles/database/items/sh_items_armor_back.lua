58c73b7508461844c10e0e84ff8876ce4e6e7bf2
VeXan39231 <parkerno552@yahoo.com>
2014-03-15 20:42:46 -0500

    Fixed Backsack levels

--- gamemode/core/sharedfiles/database/items/sh_items_armor_back.lua
+++ gamemode/core/sharedfiles/database/items/sh_items_armor_back.lua
@@ -20,6 +20,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 10)
 Item.Weight = 0
 Item.SellPrice = 100
+Item.Level = 10
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack2", "Back Sack 2", "Upgrades your storage-space by 20 slots.", "icons/amor_junkyarmor")
@@ -28,6 +29,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 20)
 Item.Weight = 0
 Item.SellPrice = 200
+Item.Level = 20
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack3", "Back Sack 3", "Upgrades your storage-space by 30 slots.", "icons/amor_junkyarmor")
@@ -36,6 +38,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 30)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 30
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack4", "Back Sack 4", "Upgrades your storage-space by 40 slots.", "icons/amor_junkyarmor")
@@ -44,6 +47,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 40)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 40
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack5", "Back Sack 5", "Upgrades your storage-space by 50 slots.", "icons/amor_junkyarmor")
@@ -52,6 +56,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 50)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 50
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack6", "Back Sack 6", "Upgrades your storage-space by 60 slots.", "icons/amor_junkyarmor")
@@ -60,6 +65,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 60)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 60
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack7", "Back Sack 7", "Upgrades your storage-space by 70 slots.", "icons/amor_junkyarmor")
@@ -68,6 +74,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 70)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 70
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack8", "Back Sack 8", "Upgrades your storage-space by 80 slots.", "icons/amor_junkyarmor")
@@ -76,6 +83,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 80)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 80
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack9", "Back Sack 9", "Upgrades your storage-space by 90 slots.", "icons/amor_junkyarmor")
@@ -84,6 +92,7 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 90)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 90
 Register.Item(Item)
 
 local Item = QuickCreateItemTable(BaseArmor, "armor_back_sack10", "Ultimate Back Sack", "Upgrades your storage-space by 100 slots.", "icons/amor_junkyarmor")
@@ -92,4 +101,5 @@ Item = AddStats(Item, "slot_back", 0)
 Item = AddBuff(Item, "stat_maxweight", 100)
 Item.Weight = 0
 Item.SellPrice = 0
+Item.Level = 100
 Register.Item(Item)
